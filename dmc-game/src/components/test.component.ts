f916ada350748948574935bcf4b5f288bd85cdd4
Hoangvu1520 <109757287+Hoangvu1520@users.noreply.github.com>
2025-08-03 18:09:44 +0700

    update

---
+++ dmc-game/src/components/test.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'user-profile',
+  template: `
+    <h1>User profile</h1>
+    <p>This is the user profile page</p>
+  `,
+})
+export class UserProfile { /* Your component code goes here */ }
