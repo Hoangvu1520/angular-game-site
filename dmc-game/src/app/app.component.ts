8f6b11176edb32c2f05014adc189dd074259cc87
Hoangvu1520 <109757287+Hoangvu1520@users.noreply.github.com>
2026-06-11 16:44:27 +0700

    update

--- dmc-game/src/app/app.component.ts
+++ dmc-game/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './components/Layout';
+import { HeaderComponent } from './components/Layout';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
