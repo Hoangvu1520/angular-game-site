27038524f30836e6504f83c954d59968a8e400c3
Hoangvu1520 <109757287+Hoangvu1520@users.noreply.github.com>
2026-06-13 10:47:00 +0700

    update

--- dmc-game/src/app/components/input/input.component.ts
+++ dmc-game/src/app/components/input/input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-input',
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
   templateUrl: './input.component.html',
   styleUrl: './input.component.scss'
 })
+export type InputType = "text" | "email" | "phoneNumber"
 export class InputComponent {
-
+  @Input() isRequired?: boolean;
+  @Input() placeholderText: string = '';
+  @Input() inputType: InputType = 'text';
 }
