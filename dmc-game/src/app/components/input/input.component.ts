6ca33003cf123e1b3af3c19dffddab86914fba7c
Hoangvu1520 <109757287+Hoangvu1520@users.noreply.github.com>
2026-06-13 15:46:02 +0700

    update

--- dmc-game/src/app/components/input/input.component.ts
+++ dmc-game/src/app/components/input/input.component.ts
@@ -1,14 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Renderer2 } from '@angular/core';
+import { NgControl } from '@angular/forms';
+
+export type InputType = "text" | "email" | "phoneNumber"
 
 @Component({
   selector: 'app-input',
+  standalone:true,
   imports: [],
   templateUrl: './input.component.html',
   styleUrl: './input.component.scss'
 })
-export type InputType = "text" | "email" | "phoneNumber"
+
 export class InputComponent {
   @Input() isRequired?: boolean;
   @Input() placeholderText: string = '';
   @Input() inputType: InputType = 'text';
+
+  public value: any;
+  private onChange = (value: any) => {};
+  private onTouched = () => {};
+
+  constructor(private renderer:Renderer2, public ngControl: NgControl){
+    
+  }
 }
