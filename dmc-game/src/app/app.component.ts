e4292abc0024eb5ebb59e14157853e8e878647e5
Hoangvu1520 <109757287+Hoangvu1520@users.noreply.github.com>
2026-06-16 09:48:34 +0700

    update

--- dmc-game/src/app/app.component.ts
+++ dmc-game/src/app/app.component.ts
@@ -2,13 +2,24 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './components/Layout';
 import { HeaderComponent } from './components/Layout';
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent],
+  // templateUrl: './app.component.html',
+  template:`
+  <main>
+    <header class="brand-name">
+      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+    </header>
+    <section class="content">
+      <app-home></app-home>
+    </section>
+  </main>
+`,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'dmc-game';
+  title = 'Home';
 }
