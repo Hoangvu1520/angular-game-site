5a86cb406fad6c4d7c68370dac40d1935301d133
Hoangvu1520 <109757287+Hoangvu1520@users.noreply.github.com>
2026-06-19 12:11:52 +0700

    update

--- dmc-game/src/app/app.component.ts
+++ dmc-game/src/app/app.component.ts
@@ -3,23 +3,25 @@ import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './components/Layout';
 import { HeaderComponent } from './components/Layout';
 import { HomeComponent } from './pages/home/home.component';
+import { WishItem } from '../shared/models/wishList';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent],
-  // templateUrl: './app.component.html',
-  template:`
-  <main>
-    <header class="brand-name">
-      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
-    </header>
-    <section class="content">
-      <app-home></app-home>
-    </section>
-  </main>
-`,
+  templateUrl: './app.component.html',
+//   template: `
+//   <main>
+//     <header class="brand-name">
+//       <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+//     </header>
+//     <section class="content">
+//       <app-home></app-home>
+//     </section>
+//   </main>
+// `,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  items = [new WishItem("hello"), new WishItem("hi", true), new WishItem("goodbye")];
   title = 'Home';
 }
