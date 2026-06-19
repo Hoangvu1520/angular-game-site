333e3efba840d139c9f9b1afc9feb0067bdf1a8d
Hoangvu1520 <109757287+Hoangvu1520@users.noreply.github.com>
2026-06-19 16:22:42 +0700

    update

--- dmc-game/src/app/app.component.ts
+++ dmc-game/src/app/app.component.ts
@@ -3,23 +3,26 @@ import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './components/Layout';
 import { HeaderComponent } from './components/Layout';
 import { HomeComponent } from './pages/home/home.component';
+import { WishList } from './shared/models/wishList';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent],
-  // templateUrl: './app.component.html',
-  template:`
-  <main>
-    <header class="brand-name">
-      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
-    </header>
-    <section class="content">
-      <app-home></app-home>
-    </section>
-  </main>
-`,
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent, CommonModule],
+  templateUrl: './app.component.html',
+//   template:`
+//   <main>
+//     <header class="brand-name">
+//       <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+//     </header>
+//     <section class="content">
+//       <app-home></app-home>
+//     </section>
+//   </main>
+// `,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  items: WishList[] = []
   title = 'Home';
 }
---
+++ dmc-game/src/app/shared/models/wishList.ts
@@ -0,0 +1,6 @@
+export class WishList {
+  constructor(
+    public task: string,
+    public completed: boolean = false,
+  ) {}
+}
