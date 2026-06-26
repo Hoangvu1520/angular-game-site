e3696f56bd7075d615d3315e6a559ccdc4591e35
Hoangvu1520 <109757287+Hoangvu1520@users.noreply.github.com>
2026-06-26 08:02:17 +0700

    update

--- backend/migrations/20260626002227_create_users_table.js
+++ backend/migrations/20260626002227_create_users_table.js
@@ -2,14 +2,24 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+    table.uuid("id").primary().defaultTo(knex.fn.uuid());
+    table.string("email").notNullable().unique();
+    table.string("phone").nullable().unique();
+    table.string("password").notNullable();
+    table.string("user_name").notNullable();
+    table.string("address").nullable();
+    table.jsonb("ext").nullable();
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.timestamp("updated_at").defaultTo(knex.fn.now());
+    table.timestamp("deleted_at").nullable();
+    table.boolean("verify").nullable().defaultTo(false);
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+    await knex.schema.dropTable("users");
 };
