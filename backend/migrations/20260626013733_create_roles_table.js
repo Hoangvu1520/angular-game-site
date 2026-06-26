77190aec2a45fde9f2ef669e05a39a7258ba3230
Hoangvu1520 <109757287+Hoangvu1520@users.noreply.github.com>
2026-06-26 08:37:59 +0700

    update

---
+++ backend/migrations/20260626013733_create_roles_table.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema.createTable('roles', function (table) {
+        table.uuid('id').primary().defaultTo(knex.fn.uuid());
+        table.string("title").unique().notNullable();
+        table.string('name').unique().notNullable();
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.timestamp('updated_at').defaultTo(knex.fn.now());
+        table.timestamp('deleted_at').nullable();
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    await knex.schema.dropTable('roles');
+};
