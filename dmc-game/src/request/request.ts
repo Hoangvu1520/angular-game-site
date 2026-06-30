81ea1954f871639d75dd3a60fb1d0f01e94c2916
Hoangvu1520 <109757287+Hoangvu1520@users.noreply.github.com>
2026-06-30 18:35:07 +0700

    update

--- dmc-game/src/request/request.ts
+++ dmc-game/src/request/request.ts
@@ -1,3 +1,15 @@
-export const requestAPI = async () => {
-    
+export const requestAPI = async (url: string, body: any, mothod: "POST" | "GET" | "PUT" | "PATCH" | "DELETE") => {
+    const token = localStorage.getItem("accessToken");
+    const response = await fetch(process.env.NEXT_PUBLIC_BASE_API + url,
+        {
+            method: method ? method : "POST",
+            headers: {
+                "Cache-Control": "no-cache",
+                "Content-Type": "application/json",
+                // "Access-Control-Allow-Origin": "*",
+                'Authorization': token ? `Bearer ${token}` : ''
+            },
+            credentials: 'include',
+            body: method == "GET" ? undefined : JSON.stringify(body),
+        })
 };
