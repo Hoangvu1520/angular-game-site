b61e727a81c464d987f0aa47b488bad89e507e39
Hoangvu1520 <109757287+Hoangvu1520@users.noreply.github.com>
2026-07-27 08:10:30 +0700

    update

--- web/components/Layout/Footer/Footer.tsx
+++ web/components/Layout/Footer/Footer.tsx
@@ -1,4 +1,5 @@
 import React, { ReactNode, useState, useEffect } from 'react'
+import styles from "./Footer.module.scss"
 
 interface section1 {
     icon?: string;
@@ -20,7 +21,20 @@ const Footer: React.FC = () => {
     //useEffect
 
     //functions to render
-
+    const feedback = () => {
+        return (
+            <div
+                className={[
+                    "row",
+                    "justify-center",
+                    "align-center",
+                    styles.FeedBack,
+                ].join(" ")}
+            >
+                <div>Phản hồi trang web? Hãy cho chúng tôi biết ▸</div>
+            </div>
+        );
+    };
     //MAIN RENDER
     return (
         <div>Footer</div>
