ab82dcb7715f62eedebb39435bc8007021b1fb05
Hoangvu1520 <109757287+Hoangvu1520@users.noreply.github.com>
2026-08-23 01:54:11 +0700

    update

--- backend-laravel/app/Http/Controllers/AuthController.php
+++ backend-laravel/app/Http/Controllers/AuthController.php
@@ -3,10 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function Register(Request $request){
-        
+    public function register(Request $request)
+    {
+        $validated = $request->validate(["user_name" => "required|string|max:255", "email" => "required|string|email|unique:users,email"]);
+        $user = User::create(["user_name" => $validated["name"], "email" => $validated["email"], "password" => Hash::make($validated["password"])]);
+
+        //generate JWT
+        $token = $user->createToken("auth_token")->plainTextToke;
+
+        return response()->json(["success" => true, "user" => $user, "token" => $token]);
     }
 }
--- backend-laravel/routes/web.php
+++ backend-laravel/routes/web.php
@@ -1,11 +1,19 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
+});
+
+Route::post("/register", [AuthController::class, "register"]);
+
+Route::post("/login", [AuthController::class, "login"]);
+Route::get('/hello', function () {
+    return 'Hello World';
 });
 
 require __DIR__.'/settings.php';
